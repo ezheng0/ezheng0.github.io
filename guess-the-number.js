f282cdc59f86677b373349a7115946a0e47a161f
Emma Z <ezheng@ucvts.org>
2020-11-06 23:47:36 -0500

    function for checking guesses

--- guess-the-number.js
+++ guess-the-number.js
@@ -7,3 +7,15 @@ function clearGame(){
     document.getElementById("guessResponse").innerHTML = "";
     document.getElementById("replay").style.display = "none";
 }
+function valueCheck(val, min, max){
+    if(!(val >= min) || !(val <= max)){
+        return "Enter a number from 0 to 100";
+    }
+    if(val == num){
+        return `Correct, good job. You took: ${guesses.length} guesses`
+    }else if(val < num){
+        return "Too low"
+    }else if(val > num){
+        return "Too high";
+    }
+}
