60256924a0ecc514c1cacb9a417987c88a0f60b2
Emma Z <ezheng@ucvts.org>
2020-11-07 11:30:45 -0500

    fixed mistakes and rearranged file

--- guess-the-number.js
+++ guess-the-number.js
@@ -5,17 +5,18 @@ function startGame(){
     clearPage();
     num = Math.floor(Math.random() * 101);
 }
+function onGuess(){
+    let guess = document.getElementById("numberGuess").value;
+    let guessResponse = document.getElementById("guessResponse");
+    recordOnce(guess, guesses, val => !(val >= 0) || !(val <= 100));
+    respond(guessResponse, valueCheck(guess, 0, 100));
+}
+
 function clearPage(){
     document.getElementById("guessResponse").innerHTML = "";
     document.getElementById("replay").style.display = "none";
 }
-function onGuess(){
-    let guess = document.getElementById("numberGuess").value;
-    let guessResponse = document.getElementById("guessResponse");
-    valueCheck(guess, 0, 100);
-    recordOnce(guess, guesses, val => !(val >= 0) || !(val <= 100));
-    respond(guessResponse, valueCheck);
-}
+
 function valueCheck(val, min, max){
     if(!(val >= min) || !(val <= max)){
         return "Enter a number from 0 to 100";
